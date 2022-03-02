86feed269f631891bde369e76bda1006c7dd2bc4
guaqiu5 <6947640@qq.com>
2022-03-02 18:38:04 +0800

    翻转链表

---
+++ 剑指offer24翻转链表.js
@@ -0,0 +1,7 @@
+var reverseList = function(head) {
+    if (head == null || head.next == null) return head
+    let p = reverseList(head.next)
+    head.next.next = head
+    head.next = null
+    return p
+};
